1b805428e2701ace81942a958ca705c043d7464d
abiona01 <quadratwemimo@gmail.com>
2021-08-19 19:08:08 +0100

    project update

---
+++ public/script.js
@@ -0,0 +1,16 @@
+const form = document.getElementById("contact");
+
+const formEvent = form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	let mail = new FormData(form);
+	sendMail(mail);
+});
+
+const sendMail = (mail) => {
+	fetch("https://localhost:5000/send", {
+		method: "post",
+		body: mail,
+	}).then((response) => {
+		return response.json();
+	});
+};
