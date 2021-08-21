7bff89830e3a6eb26a592ff41e02464a62943d3e
abiona01 <quadratwemimo@gmail.com>
2021-08-21 13:05:47 +0100

    nodemailer restored

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const nodemailer = require("nodemailer");
+const multiparty = require("multiparty");
 const colors = require("colors");
 require("dotenv").config();
 
@@ -10,6 +12,49 @@ const app = express();
 app.use(cors({ origin: "*" }));
 
 app.use("/", express.static(process.cwd() + "/public"));
+
+const transporter = nodemailer.createTransport({
+	service: "gmail",
+	auth: {
+		user: process.env.EMAIL,
+		pass: process.env.PASS,
+	},
+});
+
+// verify connection configuration
+transporter.verify(function (error, success) {
+	if (error) {
+		console.log(error);
+	} else {
+		console.log("Server is ready to take messages");
+	}
+});
+
+app.post("/send", (req, res) => {
+	let form = new multiparty.Form();
+	let data = {};
+	form.parse(req, function (err, fields) {
+		console.log(fields);
+		Object.keys(fields).forEach(function (property) {
+			data[property] = fields[property].toString();
+		});
+		console.log(data);
+		const mail = {
+			sender: `${data.name} <${data.email}>`,
+			to: process.env.EMAIL,
+			subject: "Contact Request",
+			text: `${data.name} <${data.email}> \n${data.message}`,
+		};
+		transporter.sendMail(mail, (err, data) => {
+			if (err) {
+				console.log(err);
+				res.status(500).send("Something went wrong.");
+			} else {
+				res.status(200).send("Email successfully sent!");
+			}
+		});
+	});
+});
 
 //Index page (static HTML)
 app.route("/").get(function (req, res) {
